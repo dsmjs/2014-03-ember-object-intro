62d2e9bef957fcec3220bfc73867e60d9c24857a
toran billups <toranb@gmail.com>
2014-03-10 21:52:03 -0500

    added basic object + tests + mixin example

--- js/app.js
+++ js/app.js
@@ -11,3 +11,33 @@ App.PeopleRoute = Ember.Route.extend({
         return [first, last];
     }
 });
+
+App.Dog = Ember.Mixin.create({
+    init: function() {
+        this.set('dog', true);
+    },
+    bark: function() {
+        return "RUFF";
+    }.property()
+});
+
+App.Cat = Ember.Mixin.create({
+    init: function() {
+        this.set('cat', true);
+        this._super();
+    },
+    purr: function() {
+        return "wat";
+    }.property()
+});
+
+App.Person = Ember.Object.extend(App.Dog, App.Cat, {
+    init: function(first, last) {
+        this.set('first', first);
+        this.set('last', last);
+        this._super();
+    },
+    fullname: function() {
+        return this.get('first') + ' ' + this.get('last');
+    }.property()
+});
--- js/tests/unit_tests.js
+++ js/tests/unit_tests.js
@@ -1,1 +1,13 @@
-
+test("hello world", function() {
+    var person = new App.Person('joel', 'taddei');
+    var fullname = person.get('fullname');
+    equal('joel taddei', fullname);
+    var bark = person.get('bark');
+    equal('RUFF', bark);
+    var purr = person.get('purr');
+    equal('wat', purr);
+    var cat = person.get('cat');
+    equal(cat, true);
+    var dogg = person.get('dog');
+    equal(dogg, true);
+});
